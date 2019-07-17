e4221496436eb47dc03ea09e8bef0d200fbbfc68
ShristySk1 <srezty@gmail.com>
2019-07-17 12:44:03 +0545

    api connected bt works only in emulator

--- src/app/main/routine/routine.page.ts
+++ src/app/main/routine/routine.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-routine',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./routine.page.scss'],
 })
 export class RoutinePage implements OnInit {
-
-  constructor() { }
+posts: object;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://192.168.142.1:8080/rest/timetable').subscribe(posts => {
+
+      this.posts = posts;
+      console.log(posts);
+    });
   }
 
 }
